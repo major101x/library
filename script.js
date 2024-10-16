424ee319e266a584bbbc30d59322fe5efe1dfa2b
Olayiwola Aroyeun <72233326+major101x@users.noreply.github.com>
2024-10-16 11:23:49 +0100

    create book constructor and addBookToLibrary function

---
+++ script.js
@@ -0,0 +1,40 @@
+const library = [
+  {
+    title: "father",
+    author: "nigga",
+    numberOfPages: 122,
+    hasRead: "Want to read",
+  },
+  {
+    title: "mother",
+    author: "nigga",
+    numberOfPages: 122,
+    hasRead: "Want to read",
+  },
+  {
+    title: "sister",
+    author: "nigga",
+    numberOfPages: 122,
+    hasRead: "Want to read",
+  },
+  {
+    title: "brother",
+    author: "nigga",
+    numberOfPages: 122,
+    hasRead: "Want to read",
+  },
+];
+
+function Book(title, author, numberOfPages, hasRead) {
+  this.title = title;
+  this.author = author;
+  this.numberOfPages = numberOfPages;
+  this.hasRead = hasRead;
+}
+
+function addBookToLibrary(title, author, numberOfPages, hasRead) {
+  const book = new Book(title, author, numberOfPages, hasRead);
+  library.push(book);
+  console.log(library);
+  return library;
+}
